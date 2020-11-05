f71c66f2803e4ec31a54a0aae0bbc556b217731f
unknown <ornitmen@gmail.com>
2020-11-05 18:41:06 +0200

    WORK32

---
+++ WORK32/WORK32.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x;
+    double a;
+    double b;
+    double c;
+    double F;
+
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    cout << "c = ";
+    cin >> c;
+    cout << "x = ";
+    cin >> x;
+    
+    // 1 Спосіб
+
+    if (x < 0 && b != 0) 
+    {
+        F = a - x / (10 + b);
+    }
+    if (x > 0 && b == 0) 
+    {
+        F = (x - a) / (x - c);
+    }
+    else 
+    {
+        F = 3 * x + 2 / c;
+    }
+    cout << "F = " << F<<endl;
+
+    // 2 Спосіб
+
+    if (x < 0 && b != 0) 
+    {
+        F = a - x / (10 + b);
+    }
+    else 
+    {
+        if (x > 0 && b == 0) 
+        {
+            F = (x - a) / (x - c);
+        }
+        else 
+        {
+            F = 3 * x + 2 / c;
+        }
+
+    }
+    cout << "F = " << F;
+}
